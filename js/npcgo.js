4c3e0e983e6259b9d396e97412d3e08f5fe0ccf3
solarias <ansewo@naver.com>
2016-09-04 23:38:04 +0900

    124

--- js/npcgo.js
+++ js/npcgo.js
@@ -7,17 +7,19 @@ var mediaConfig =  { video: true };
 var errBack = function(e) {
 	console.log('An error has occurred!', e);
 };
+var text = "";
 
 navigator.mediaDevices.enumerateDevices()
 .then(function(devices) {
   devices.forEach(function(device) {
-    alert(device.kind + ": " + device.label +
+    console.log(device.kind + ": " + device.label +
                 " id = " + device.deviceId);
   });
 })
 .catch(function(err) {
-  alert(err.name + ": " + err.message);
+ console.log(err.name + ": " + err.message);
 });
+alert(text);
 
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     // Not adding `{ audio: true }` since we only want video now
