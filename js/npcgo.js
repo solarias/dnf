12da1ee47628421c09731e628124883d41b831bc
solarias <ansewo@naver.com>
2016-09-05 16:33:50 +0900

    NPC고 - 그럴 듯하게

--- js/npcgo.js
+++ js/npcgo.js
@@ -8,6 +8,16 @@ var cameraElement =  $("#camera");
 var cameraList = [];
 var wi = window.innerWidth;
 var wh = window.innerHeight;
+var sw = 0;
+var music = new Audio("./sound/underfoot.mp3");
+    music.volume = 0.3;
+    music.loop = true;
+var music2 = new Audio("./sound/beckey_bgm.mp3");
+    music2.volume = 0.3;
+    music2.loop = true;
+var shot = new Audio("./sound/shot.mp3");
+var miss = new Audio("./sound/miss.mp3");
+var out = new Audio("./sound/beckey_fail.mp3");
 
 navigator.getUserMedia = navigator.getUserMedia ||
   navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
@@ -59,8 +69,55 @@ function start() {
   navigator.getUserMedia(constraints, successCallback, errorCallback);
 }
 
-setTimeout(function() {
-    alert("테스트 개시");
-    start();
-    window.navigator.vibrate(200);
-},1000);
+function shoot() {
+    if ($("#ball").offsetTop > window.innerHeight * 0.25) {
+        $("#ball").style.top = ($("#ball").offsetTop - 8).toString() + "px";
+        setTimeout(function() {
+            shoot();
+        },16);
+        return;
+    } else {
+        $("#ball").style.top = (window.innerHeight * 0.8).toString() + "px";
+        if (Math.random() < 0.98) {
+            miss.pause();
+            miss.currentTime = 0;
+            miss.play();
+            sw = 0;
+            $("#open").disabled = false;
+        } else {
+            $("#npc").style.visibility = "hidden";
+            $("#miss").style.visibility = "hidden";
+            out.play();
+            $("#open").innerHTML = "포획 완료!";
+        }
+    }
+}
+
+window.onload = function() {
+    setTimeout(function() {
+        alert("길거리를 돌아다니는 NPC를 탐색합니다.");
+        music.play();
+        start();
+        setTimeout(function() {
+            $("#open").disabled = false;
+            music.pause();
+            music2.play();
+            $("#npc").style.visibility = "visible";
+            $("#miss").style.visibility = "visible";
+            window.navigator.vibrate(500);
+            $("#open").onclick = function() {
+                if (sw === 0) {
+                    sw = 1;
+                    $("#open").disabled = true;
+                    shot.pause();
+                    shot.currentTime = 0;
+                    shot.play();
+                    shoot();
+                }
+            };
+            $("#open").disbled = false;
+            $("#open").innerHTML = "포획하기";
+            alert("야생의 황녀 에르제이(가) 나타났다!");
+        },15000);
+    },100);
+};
