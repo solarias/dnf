dafcd30a24e0cebdb737562bf523e348cdb943fe
solarias <ansewo@naver.com>
2016-09-14 01:16:44 +0900

    123

--- js/terago.js
+++ js/terago.js
@@ -301,7 +301,7 @@ function getItem(address) {
     var tmpArr = [];
     var itm;
     for (var i=0;i<itemList.length;i++) {
-        if (temp_type === itemList[i]["sort1"] && temp_level.indexOf(itemList[i]["level"]) > 0) {
+        if (temp_type === itemList[i]["sort1"] && temp_level.indexOf(itemList[i]["level"]) >= 0) {
             tmpArr.push(itemList[i]);
         }
     }
